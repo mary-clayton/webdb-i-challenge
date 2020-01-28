017079991c08594eaa48639b8d0031cfdd842ae4
Mary <mary.clayton1@hotmail.com>
2020-01-27 22:27:11 -0700

    finished queries in SQL; MVP 50% completed

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const server = require('./server.js');
 
+require('dotenv').config()
+
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
+//(server.listen) ? dotenv : PORT 4000
