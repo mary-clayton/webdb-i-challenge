175ea64eab7acaebaa3cbb8320b7c659d17980e5
Mary <mary.clayton1@hotmail.com>
2020-01-28 22:56:01 -0700

    mvp completed

--- accountRouter.js
+++ accountRouter.js
@@ -18,37 +18,46 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
+    const {id} = req.params
     try {
-
+        const account = await db('accounts').where('id', id)
+        res.json(account);
     } catch (err) {
-        
+        res.status(500).json({ message: 'failed to get account info with specific id' })
     }
 })
 
 // POST
 router.post('/', async (req, res) => {
+    const accountData = req.body;
     try {
-
+        const account = await db('accounts').insert(accountData);
+        res.status(201).json(account)
     } catch (err) {
-        
+        res.status(500).json({ message: 'failed to create account info' })
     }
 })
 
 // UPDATE
 router.put('/:id', async (req, res) => {
+    const {id} = req.params
+    const accountData = req.body
     try {
-
+        const accountUpdated = await db('accounts').where('id', id).update(accountData);
+        res.status(200).json({ updated: accountUpdated });
     } catch (err) {
-        
+        res.status(500).json({ message: 'failed to update account info with specific id' })
     }
 })
 
 // DELETE 
 router.delete('/:id', async (req, res) => {
+    const {id} = req.params
     try {
-
+        const accountDeleted = await db('accounts').where('id', id).del();
+        res.json({ deletedRecords: accountDeleted })
     } catch (err) {
-        
+        res.status(500).json({ message: 'failed to delete account info with specific id' })
     }
 })
 
